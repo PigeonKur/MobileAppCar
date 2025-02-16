859b8a679d23121e3d39c8504d9b010770a76379
PigeonKur <ggnn3974@gmail.com>
2025-02-16 16:26:38 +0300

    ready Splash, Login, register screens

---
+++ app/src/main/java/com/example/myapplication/screen/HomeScreen.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+    @Composable
+    fun HomeScreen() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Добро пожаловать!", style = MaterialTheme.typography.headlineLarge, color = Color.Black)
+        }
+    }
---
+++ app/src/main/java/com/example/myapplication/screen/LoginScreen.kt
@@ -0,0 +1,88 @@
+package com.example.myapplication.screen
+
+import android.text.TextUtils
+import android.view.View
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.isPopupLayout
+import androidx.navigation.NavController
+import com.example.myapplication.Methods.isEmailValid
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isEmailValid by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = email,
+            onValueChange = {email = it
+                isEmailValid = email.isEmailValid()},
+            label = { Text("Email") },
+            isError = !isEmailValid
+        )
+        if (!isEmailValid) {
+            Text(text = "Некорректный email", color = Color.Red)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Пароль") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                if (email.isEmailValid() && password.isNotEmpty()) {
+                    navController.navigate("home"){
+                        popUpTo("login") {
+                            inclusive = true
+                        }
+                    }
+                }
+            },
+            enabled = email.isEmailValid() && password.isNotEmpty()
+        ) {
+            Text("Войти")
+        }
+
+        TextButton(onClick = { navController.navigate("register") }) {
+            Text("Регистрация")
+        }
+    }
+}
+
+
+
