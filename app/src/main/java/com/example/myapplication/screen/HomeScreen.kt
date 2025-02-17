9a75d8d485a25822c141122795e23e3edbec9d45
PigeonKur <ggnn3974@gmail.com>
2025-02-17 22:10:24 +0300

    Develop homeScreen

--- app/src/main/java/com/example/myapplication/screen/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/screen/HomeScreen.kt
@@ -1,20 +1,83 @@
 package com.example.myapplication.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-    @Composable
+@Composable
     fun HomeScreen() {
+    var showMessage by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Text(text = "Добро пожаловать!", style = MaterialTheme.typography.headlineLarge, color = Color.Black)
+            Image(
+                painter = painterResource(R.drawable.car1),
+                contentDescription = "Car_1",
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Автомобиль 1", color = Color.Black, fontSize = 18.sp)
+            Text(text = "Цена 1", color = Color.Black, fontSize = 18.sp)
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {
+            showMessage = true
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(3200)
+                showMessage = false
+            }
+        }) {
+            Text(text = "Арендовать автомобиль")
+        }
+
+        if (showMessage) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Автомобиль арендован", color = Color.Green)
         }
     }
+}
